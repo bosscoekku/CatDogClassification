52d6015f4609b1128b611d0781776a39f5896854
kritsada.kong-a-sa@wdc.com <Kritsada.Kong-A-Sa@wdc.com>
2020-03-17 08:18:51 +0700

    asdasa

--- app.py
+++ app.py
@@ -98,4 +98,5 @@ def upload_image():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
