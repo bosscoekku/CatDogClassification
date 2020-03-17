036254d41f10cdaf6c6b24a1de98feea158f6621
kritsada.kong-a-sa@wdc.com <kritsada.kong-a-sa@wdc.com>
2020-03-17 22:38:16 +0700

    update

--- app.py
+++ app.py
@@ -1,102 +1,106 @@
-
 import os
+import numpy as np  
+from flask import Flask, render_template, session, redirect, url_for, session,request
+from flask_wtf import FlaskForm
+#from PIL import Image
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import secure_filename
 
-from flask import Flask, render_template, session, redirect, url_for, session
-from flask_wtf import FlaskForm
-from wtforms import TextField,SubmitField
-from wtforms.validators import NumberRange
-
-import numpy as np  
-from tensorflow.keras.models import load_model
-import joblib
 
 
 
-def return_prediction(model,scaler,sample_json):
-    
-    # For larger data features, you should probably write a for loop
-    # That builds out this array for you
-    
-    s_len = sample_json['sepal_length']
-    s_wid = sample_json['sepal_width']
-    p_len = sample_json['petal_length']
-    p_wid = sample_json['petal_width']
-    
-    flower = [[s_len,s_wid,p_len,p_wid]]
-    
-    flower = scaler.transform(flower)   
-    
-    classes = np.array(['setosa', 'versicolor', 'virginica'])
-    
-    class_ind = model.predict_classes(flower)
-    
-    return classes[class_ind][0]
+def allowed_image(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 
+def allowed_image_filesize(filesize):
 
+    if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
+        return True
+    else:
+        return False
+
+def load_imagefrom(img_path):
+    img = image.load_img(img_path, target_size=(128, 128))
+    img_tensor = image.img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+
+    return img_tensor
+
+    
 app = Flask(__name__)
 # Configure a secret SECRET_KEY
 # We will later learn much better ways to do this!!
 app.config['SECRET_KEY'] = 'mysecretkey'
-
+app.config["IMAGE_UPLOADS"] = "static"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
 
 # REMEMBER TO LOAD THE MODEL AND THE SCALER!
-flower_model = load_model("final_iris_model.h5")
-flower_scaler = joblib.load("iris_scaler.pkl")
-
-
-# Now create a WTForm Class
-# Lots of fields available:
-# http://wtforms.readthedocs.io/en/stable/fields.html
-class FlowerForm(FlaskForm):
-    sep_len = TextField('Sepal Length')
-    sep_wid = TextField('Sepal Width')
-    pet_len = TextField('Petal Length')
-    pet_wid = TextField('Petal Width')
-
-    submit = SubmitField('Analyze')
+catdog_model = load_model("cat_dog_detector_finetune.h5")
 
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-
-    # Create instance of the form.
-    form = FlowerForm()
-    # If the form is valid on submission (we'll talk about validation next)
-    if form.validate_on_submit():
-        # Grab the data from the breed on the form.
-
-        session['sep_len'] = form.sep_len.data
-        session['sep_wid'] = form.sep_wid.data
-        session['pet_len'] = form.pet_len.data
-        session['pet_wid'] = form.pet_wid.data
-
-        return redirect(url_for("prediction"))
 
 
-    return render_template('home.html', form=form)
 
-@app.route("/upload")
-def upload():
+
+
+@app.route("/forward", methods=["GET", "POST"])
+def move_forward():
     return render_template('upload.html')
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template('upload.html')
+
+
+@app.route("/upload_image", methods=["GET", "POST"])
+def upload_image():
+  
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url)
+
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                path_img = os.path.join(app.config["IMAGE_UPLOADS"], filename)
+                print(path_img)
+                image.save(path_img)
+                print("Image saved")
+                new_image = load_imagefrom(path_img)
+                # check prediction
+                classes = catdog_model.predict(new_image)
+                print(f"Class accuracy :{classes[0][0]*100.0} %",)
+                if classes[0][0]*100.0>50:
+                    results = "dog"
+                else:
+                    results = "cat"
+
+            else:
+                print("That file extension is not allowed")
+                return "<h1 style='color: red;'>That file extension is not allowed!</h1>"
     
-@app.route('/prediction')
-def prediction():
-
-    content = {}
-
-    content['sepal_length'] = float(session['sep_len'])
-    content['sepal_width'] = float(session['sep_wid'])
-    content['petal_length'] = float(session['pet_len'])
-    content['petal_width'] = float(session['pet_wid'])
-
-    results = return_prediction(model=flower_model,scaler=flower_scaler,sample_json=content)
-
-    return render_template('prediction.html',results=results)
+    return render_template('prediction.html',results=results,pathImage = path_img)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run()
