8266300d99929d9f07f7a8b9b87151f25dea5ff1
kritsada.kong-a-sa@wdc.com <kritsada.kong-a-sa@wdc.com>
2020-05-19 00:41:03 +0700

    fixed logging

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import sys
 import os
 import cv2
 import numpy as np  
+from loguru import logger
 from tensorflow.keras.models import load_model
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, session, redirect, url_for, session,request,jsonify
@@ -14,17 +16,27 @@ app.config["IMAGE_UPLOADS"] = "static"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
 
+# config logging
+logger.add("file_model.log")
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
+
 # REMEMBER TO LOAD THE MODEL AND THE SCALER!
-model_malaria = load_model("modelAddDataAug200Epoc.h5")
+modelName = "modelAddDataAug200Epoc.h5"
+model_malaria = load_model(modelName)
+
+
+logger.info("Start with model name is {}".format(modelName))
+
+
 
 
 def allowed_image(filename):
-
+    logger.debug("Checking filename is {}".format(filename))
     if not "." in filename:
         return False
 
     ext = filename.rsplit(".", 1)[1]
-
+    logger.debug("Checking extion file  is {}".format(ext))
     if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
         return True
     else:
@@ -32,7 +44,7 @@ def allowed_image(filename):
 
 
 def allowed_image_filesize(filesize):
-
+    logger.debug("Checking filesize is {}".format(filesize))
     if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
         return True
     else:
@@ -42,6 +54,7 @@ def allowed_image_filesize(filesize):
 
 # load image
 def load_cv2(img_path):
+    logger.debug("Path image is {}".format(img_path))
     img_data = cv2.imread(img_path)
     img_data = cv2.resize(img_data,(118,118))
     img_data = img_data/255.0
@@ -66,27 +79,26 @@ def upload_image():
             image = request.files["image"]
 
             if image.filename == "":
-                print("No filename")
+                logger.debug("No filename and redirect to homepage")
                 return redirect(request.url)
 
             if allowed_image(image.filename):
                 filename = secure_filename(image.filename)
                 path_img = os.path.join(app.config["IMAGE_UPLOADS"], filename)
-                print(path_img)
                 image.save(path_img)
-                print("Image saved")
+                logger.info("Save image succesful")
                 new_image = load_cv2(path_img)
                 # check prediction
                 classes = model_malaria.predict(new_image)
                 msg_result = f"Class probability :{format(classes[0][0]*100.0,'.2f')} %"
-                print(msg_result)
+                logger.info(msg_result)
                 if classes[0][0]*100.0<50:
                     results = "Cat"
                 else:
                     results = "Dog"
 
             else:
-                print("That file extension is not allowed")
+                logger.debug("That file extension is not allowed")
                 return "<h1 style='color: red;'>That file extension is not allowed!</h1>"
     
     return render_template('prediction.html',results=results,pathImage = path_img,msg = msg_result)
@@ -96,4 +108,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
